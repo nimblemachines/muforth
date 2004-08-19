30ac0d8fd30b708d59cd07dbf48c1305a58a9744
Randy Thelen <rthelen@madscientistroom.org>
2004-08-19 07:47:25 +0000

    I've added a ton of comments and some possible commands.  I'm not completely convinced it's correctly designed.  But, I'm sure it's not too far off. I've also started the local nub.  When I get the two beginning communication I'll be sure to check them both in.
    git-svn-id: file:///home/david/tashtego/svn/trunk/muforth@231 f1bc7e2d-46f1-0310-96be-f91141564092

--- debugger.c
+++ debugger.c
@@ -14,7 +14,132 @@
 static void simple_debugger(void);
 extern cell_t snprint_func_name(char *str, int size, cell_t addr);
 
+/**********************************************
+ *
+ * debugger structure
+ *
+ * The debugger is predicated on the theory that it knows very little
+ * about the target machine.  Therefore, it querries the target machine
+ * for the following information:
+ * 1) heap base, heap length -- These are the dictionary heaps
+ * 2) stack base, stack length -- The region of the data stack
+ * 3) return stack base, length -- Return stack region
+ * 4) read/set muForth registers -- PC, SP, RP
+ * 5) read processor architecture -- The debugger doesn't even know that!
+ * 6) read/set processor specific registers -- Once the cpu is known then processor
+ *    specific registers can be read/set
+ * 7) read/set memory
+ *
+ * And, my theory is that I'll implement this simply enough that
+ * we can port it to muForth soon.
+ *
+ * To send commands to the target, call through the dbg_send vector.
+ * To pick up responses from the target, call through the dbg_recv vector.
+ * These vectors need to be implemented by by routines that can send
+ * and receive bytes to/from the target machine.
+ *
+ * On the target machine there needs to be a nub.  The nub responds to
+ * commands and sends responds.  The nub does not initiate new commands
+ * to the host.  The only exception to that rule is that the nub
+ * will send data to the host indicating that the target is in the debugger.
+ *
+ * If this all feels like gdb and its nub that's because I've modeled this after
+ * that structure.
+ *
+ * Oh, here's the big fun.  There are two data stacks and two return stacks.
+ * This is because when we're debugging local code, we can't use the same stack
+ * that is currently in use.  Therefore we have to switch stacks.  However,
+ * when we're debugging a target machine then we don't need to use a separate
+ * stack.  What we want here are two ways to enter the debugger.  In one
+ * case we'll have entered the debugger because code on the local machine
+ * excepted or tripped some how.  The other entry is when we want to connect
+ * to the target.  In that case there is no need to switch stacks (in fact,
+ * switching stacks there would be wrong).
+ *
+ * The trigger will be if we are entered via a call to mu_dbg() or via the
+ * word mu_dbg_connect().  Other than that, they're identical implementations.
+ * 
+ * One of my ideas is to send muForth-like commands over the wire.  In this mode
+ * an entire command would be sent as a packet.  The reason this is important is
+ * the sender cannot send one packet and then later send another packet because
+ * the target will be waiting for the first command.  But, after that command
+ * arrives, it isn't going to wait for additional commands.  It's going to
+ * execute those commands and then generate a response.  That response will start
+ * going out over the wire.  The host had better be listening for the response!
+ * Another way to look at this dilemma is that the host may have the benefit of
+ * an O/S with processes to ship data over to the target, but the target does not.
+ * The target is single threaded and -either- listening for the request or
+ * responding to one.  Period.
+ *
+ * Given that idea, here are my target->nub request commands:
+ *
+ * endian?
+ * cpu-arch?
+ * heap-base?
+ * heap-end?
+ * stack-base?
+ * stack-end?
+ *
+ * Already I'm sensing trouble down this path.  The problem is that the
+ * text of the commands is long.  This means that the nub will have lots
+ * of bytes devoted to the parsing the commands.  Shorter commands is better.
+ * 
+ * Given that, here's another idea: short one byte commands.
+ *
+ * Here are my proposed commands:
+ *   One byte commands:
+ *     i   -- Fetch the target machine's information.
+ *            The response to this is a pretty long string of terms
+ *            each separated by semicolon's:
+ *         b or l   -- big or little endian
+ *         cpu-arch -- a short text string identifying the cpu arch:
+ *                     i386, arm7, mippy, 6809, 68000, etc.
+ *         dram<base>,<length>  -- This is a declarative of the range
+ *                                 of dram on the machine.  There may
+ *                                 be multiple such declaratives.  Also,
+ *                                 these may not be exaustive.
+ *         heap<base>,<length>  -- The range of the heap.  Searches for
+ *                                 stuff in the heap won't extend beyond
+ *                                 these boundaries.
+ *         stack<base>,<length> -- The stack range.  In this case <base> is
+ *                                 the address when there are no elements
+ *                                 on the stack.  <length> indicates the
+ *                                 number of bytes available for the stack
+ *                                 to grow.  Also, if <length> is an
+ *                                 empty string, then the stack is presumed
+ *                                 to be able to grow indefinitely, which
+ *                                 will likely be the case.
+ *         rstack<base>,<length> -- The return stack range.  All said about
+ *                                 the data stack, above, is true here.  In
+ *                                 addition the rstack and the stack may
+ *                                 have overlapping ranges.  Some systems are
+ *                                 designed this way.
+ *         pc<addr>              -- The current program counter.
+ *         sp<addr>              -- The current stack pointer
+ *         rp<addr>              -- The current return pointer
+ *         <cpu-reg><value>      -- Some number (possibly 0) of cpu specific
+ *                                  registers and their current values.
+ *
+ *     c   -- continue execution form here
+ * 
+ * Other commands:
+ *
+ * m<addr>,<length>;   -- read memory at <addr> for <length> bytes.
+ * M<addr>:<datum>[,<datum>]*; -- set memory at <addr> with datum.
+ *                                additiona data is placed at successive
+ *                                addresses.
+ * 
+ */
+
+/*
+ * The debugger is vectored, so you can implement
+ * your own debugger if you wish.
+ * Use the muForth word "debugger" to place the address of this
+ * pointer onto the stack.
+ */
 cell_t debugger;
+cell_t dbg_send;
+cell_t dbg_recv;
 
 /*
  * dbg()
