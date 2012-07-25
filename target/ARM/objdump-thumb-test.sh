31dbefb488e74952d98c1be3dfaef2ffc1482054
David Frech <david@nimblemachines.com>
2012-07-25 16:40:58 -0700

    A simple test of ARM objdump disassembly
    By generating a .s file with every possible 16-bit Thumb instruction,
    and a subset of the 32-bit Thumb instructions, assembling it (with
    arm-none-eabi-as), and then disassembling (with arm-none-eabi-objdump),
    we can compare our work on the ARM assembler and disassembler with the
    binutils versions.

---
+++ target/ARM/objdump-thumb-test.sh
@@ -0,0 +1,13 @@
+# Test disassembly of Thumb code via ARM objdump by creating a file with an
+# exhaustive set of Thumb instructions (not quite exhaustive in the 32-bit
+# case, but at least _representative_), assembled with arm-none-eabi-as and
+# disassembled with arm-none-eabi-objdump.
+
+# Create asm file
+./muforth -f target/ARM/all-thumbs.mu4 cr bye > all-thumb16.s
+
+# Assemble
+arm-none-eabi-as -o all-thumb16.o all-thumb16.s
+
+# Disassemble
+arm-none-eabi-objdump -d all-thumb16.o > all-thumb16.objdump
