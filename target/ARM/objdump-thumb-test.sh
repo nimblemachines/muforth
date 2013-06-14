d770da4472d3e13ae73fa549bd8a3bac499fb0ac
David Frech <david@nimblemachines.com>
2013-06-13 17:59:37 -0700

    Generate and disasm all 16-bit thumb instructions
    This way I can compare the output of my disasm with the output of GNU
    objdump (arm-none-eabi-objdump, that is).

--- target/ARM/objdump-thumb-test.sh
+++ target/ARM/objdump-thumb-test.sh
@@ -4,10 +4,12 @@
 # disassembled with arm-none-eabi-objdump.
 
 # Create asm file
-./muforth -f target/ARM/all-thumbs.mu4 cr bye > all-thumb16.s
+./muforth -f target/ARM/all-thumbs-gas.mu4 cr bye > all-thumb16.s
 
 # Assemble
+echo Assembling
 arm-none-eabi-as -o all-thumb16.o all-thumb16.s
 
 # Disassemble
+echo Disassembling
 arm-none-eabi-objdump -d all-thumb16.o > all-thumb16.objdump
