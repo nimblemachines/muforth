17e3012cf131e6728d6ba5d99dffb10f324fdb72
David Frech <david@nimblemachines.com>
2015-11-15 00:43:23 -0800

    I meant to keep udev rules sorted for USB devices...
    This fixes an oversight.

--- scripts/make-udev-rules.sh
+++ scripts/make-udev-rules.sh
@@ -7,16 +7,16 @@
 # Create a udev rules file that matches all the USB devices we care about:
 #   0483  - ST-LINK/V2 - for STMicro ARM boards
 #   04d8  - Microchip (why? I forget ;-)
+#   0d28  - MBED/CMSIS-DAP - for anything supported by mbed
 #   15a2  - my Freescale HC08 JS16 board
 #   1cbe  - TI/Luminary Stellaris/Tiva ICDI boards
 #   c251  - Keil CMSIS-DAP - for Freescale Freedom (FRDM) boards
-#   0d28  - MBED/CMSIS-DAP - for anything supported by mbed
 
 cat <<EOT
     # udev rules to match USB devices of interest to muFORTH
 EOT
 
-for vid in 0483 04d8 15a2 1cbe c251 0d28 ; do
+for vid in 0483 04d8 0d28 15a2 1cbe c251 ; do
     cat <<EOT
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="$vid", OWNER:="$1"
 EOT
