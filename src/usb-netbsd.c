61e0215cc74d812b96a4a6a64b8aacf90945123b
David Frech <david@nimblemachines.com>
2020-01-31 17:39:38 -0800

    [core] Update copyright to 2020!
    Happy new year!

--- src/usb-netbsd.c
+++ src/usb-netbsd.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of muforth: https://muforth.nimblemachines.com/
  *
- * Copyright (c) 2002-2019 David Frech. (Read the LICENSE for details.)
+ * Copyright (c) 2002-2020 David Frech. (Read the LICENSE for details.)
  */
 
 /*
