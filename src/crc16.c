748395505e01c46ea9f4c7f9cd9b5079572e922a
David Frech <david@nimblemachines.com>
2023-09-27 17:54:09 -0600

    [C core] Added reference CRC16 implementation to C code
    It's now easy to compare the X/YMODEM reference bit-wise CRC16-CCITT
    implementation against the clever and optimized version that I found on
    an MSP430 forum.

--- src/crc16.c
+++ src/crc16.c
@@ -23,7 +23,7 @@
  * shifting and xoring the CRC accumulator with itself. In order to change
  * the polynomial, two lines of code would have to change.
  */
-static uint16_t crc16_byte(uint16_t crc, uint8_t b)
+static uint16_t crc16_byte_thierry(uint16_t crc, uint8_t b)
 {
     uint16_t x;
     x = ((crc >> 8) ^ b) & 0xff;
@@ -33,21 +33,49 @@ static uint16_t crc16_byte(uint16_t crc, uint8_t b)
     return crc;
 }
 
-static uint16_t crc16_buf(uint16_t crc, uint8_t *p, size_t len)
+/*
+ * This is the "reference" bitwise implementation from the X/YMODEM
+ * protocol reference (ymodem.txt), edited by Chuck Forsberg.
+ */
+static uint16_t crc16_byte_xmodem(uint16_t crc, uint8_t b)
 {
-    for (; len > 0; len--)
-        crc = crc16_byte(crc, *p++);
+    int i;
+
+    crc = crc ^ (b << 8);
+    for (i = 0; i < 8; ++i)
+        if (crc & 0x8000)
+            crc = (crc << 1) ^ 0x1021;
+        else
+            crc = (crc << 1);
 
     return crc;
 }
 
-void mu_c_crc16_byte(void)  /* c-crc16-byte  ( crc byte - crc' */
+/*
+ * crc16_byte points either to Thierry's implementation or to the X/YMODEM
+ * reference implementation. We default to Thierry's.
+ */
+static uint16_t (*crc16_byte)(uint16_t, uint8_t) = crc16_byte_thierry;
+
+/* Set the byte-wise CRC function. */
+void mu_c_crc16_thierry(void) { crc16_byte = crc16_byte_thierry; }
+void mu_c_crc16_xmodem(void)  { crc16_byte = crc16_byte_xmodem; }
+
+void mu_c_crc16_byte(void)  /* c-crc16-byte  ( crc byte - crc') */
 {
-    ST1 = crc16_byte(ST1, TOP);
+    ST1 = (*crc16_byte)(ST1, TOP);
     DROP(1);
 }
 
-void mu_c_crc16_buf(void)   /* c-crc16-buf   ( crc buf n - crc' */
+static uint16_t crc16_buf(uint16_t crc, uint8_t *p, size_t len)
+{
+    for (; len > 0; len--)
+        crc = (*crc16_byte)(crc, *p++);
+
+    return crc;
+}
+
+void mu_c_crc16_buf(void)   /* c-crc16-buf  ( crc buf n - crc') */
 {
     ST2 = crc16_buf(ST2, (uint8_t *)ST1, TOP);
     DROP(2);
