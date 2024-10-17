fdef5616bf1fa338f315ee6d01052cf5287011f5
David Frech <david@nimblemachines.com>
2024-10-16 21:54:00 -0700

    [core] Updated comment rewriter Lua script
    There are two ways to do block or multi-line comments in muforth:
    
    (1) Surround a block of text with "( " and ")". This was the original
    change: making all lines except for the first start with "|".
    
    (2) Using the "comment" word. It consumes the following token, and then
    goes into a loop, throwing away tokens until it finds the token again.
    
    I added support in the rewriter script for (2), and in so doing had to
    do some iteration. Several of my uses of "comment" involved using
    a token consisting of a horizontal line of chars that matched - and
    would align with - the closing use of the token.
    
    Because I am adding "| " to the beginning of every line between the two
    appearances of the framing token, the alignment can suffer. So I had to
    be careful about that.
    
    At this point, I think the script works well for both types (1) and (2).

--- scripts/comments.lua
+++ scripts/comments.lua
@@ -8,29 +8,67 @@
 -- comment or not, but for clarity I am using a separate variable,
 -- in_comment.
 function convert()
-    local in_comment = false
+    local comment_type = "none"     -- outside a comment
     local leader, leader_match, before_lparen, after_lparen
+    local token
+
     for l in io.lines() do          -- read stdin line by line
-        if not in_comment then
-            before_lparen, after_lparen = l:match "^(%s*)%((%s+)"
-            if before_lparen then
-                -- We found a left paren, but this could be a one-line
-                -- comment, so let's look for a right paren before doing
-                -- anything else.
-                if not l:match "%)" then
-                    -- No right paren; this must be a multi-line comment.
-                    in_comment = true
-                    leader       =         before_lparen .. "|" .. after_lparen
-                    leader_match = "^(" .. before_lparen .. " " .. after_lparen .. ")"
+        if comment_type == "none" then
+            -- Check for "comment" followed by a token. This begins a
+            -- token-style comment.
+            c, token = l:match "^%s*(%S+)%s+(%S+)"
+            if c and c == "comment" then
+                comment_type = "token"
+                before_lparen = ""      -- treat as if "(" in leftmost column
+            else
+                -- Maybe we have "comment" by itself on a line and the
+                -- token on the following line?
+                c = l:match "^%s*(%S+)%s*$"
+                if c and c == "comment" then
+                    -- We have to get the token from the following
+                    -- line!
+                    comment_type = "expecting a token"
+                else
+                    -- No "comment" found. Check for a left paren. This
+                    -- begins a paren-style comment.
+                    before_lparen, after_lparen = l:match "^(%s*)%((%s+)"
+                    if before_lparen then
+                        -- We found a left paren, but this could be a one-line
+                        -- comment, so let's look for a right paren before doing
+                        -- anything else.
+                        if not l:match "%)" then
+                            -- No right paren; this must be a multi-line comment.
+                            comment_type = "paren"
+                            leader       =         before_lparen .. "|" .. after_lparen
+                            leader_match = "^(" .. before_lparen .. " " .. after_lparen .. ")"
+                        end
+                    end
                 end
             end
+        elseif comment_type == "expecting a token" then
+            comment_type = "token"
+            before_lparen = ""      -- treat as if "(" in leftmost column
+
+            -- Sometimes the token is an ascii horizontal line of
+            -- chars, and the closing token is meant to align with it.
+            -- Since we are going to be indenting every line with two
+            -- chars - "| " - which will move the closing over two
+            -- spaces, let's move the *opening* token over two spaces
+            -- as well!
+            local sp
+            sp, token = l:match "^(%s*)(%S+)"
+            l = sp .. "  " .. token
         else
             -- in comment; modify line
+            -- comment_type should be "paren" or "token"
+
+            -- The empty line test works for both paren and token comment
+            -- styles.
             if l:match "^%s*$" then
                 -- Empty line or only whitespace; replace with indent and
-                -- pipe.
+                -- vertical bar.
                 l = before_lparen .. "|"
-            else
+            elseif comment_type == "paren" then
                 local changes
                 l, changes = l:gsub(leader_match, leader)
                 if changes == 0 then
@@ -51,8 +89,22 @@ function convert()
                         l = l:gsub("^(%s*)", leader)
                     end
                 end
+                if l:match "%)" then
+                    comment_type = "none"
+                end
+            elseif comment_type == "token" then
+                for w in l:gmatch "%S+" do      -- tokenize line
+                    if w == token then
+                        -- found token in the text of the line
+                        comment_type = "none"
+                    end
+                end
+                -- Prefix text with vertical bar.
+                l = "| " .. l
+            else
+                out(string.format("Comment type is %s\n", comment_type))
+                os.exit(1)
             end
-            if l:match "%)" then in_comment = false end
         end
         -- Whether or not the line was changed, write it to stdout.
         io.write(l .. "\n")
