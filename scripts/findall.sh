2d8011f07d649965b3670c6efb936862d7930980
David Frech <david@nimblemachines.com>
2026-02-23 16:36:08 -0800

    On Mac, fix sedding of copyright
    The BSD version of sed barfed on three files when I tried to updated the
    copyright dates.
    
    There were two .d4 files (in mu/target/6809/step/) that contained weird
    (non-UTF8) characters; those weird characters have been removed.
    
    In addition, there were tons of non-UTF8 characters in
    mu/target/ARM/BootStrap.s, as a result of the old du code generating
    them when trying to show the ASCII for each byte of memory. Since there
    is no copyright to be updated in *any* .s file, I removed "*.s" from the
    list of files that scripts/findall.sh feeds to find.
    
    With these two changes I was able to successfully update the copyright
    for everything.

--- scripts/findall.sh
+++ scripts/findall.sh
@@ -9,5 +9,5 @@ find . \( -name BUILDING -o -name NOTES -o -name README -o -name TODO \
        -o -name COPYRIGHT -o -name LICENSE -o -name README.obsolete \
        -o -name "*.in" -o -name "*.sh" -o -name "*.sed" -o -name "*.lua" \
        -o -name "*.c" -o -name "*.h" -o -name "*.mu4" -o -name "*.md" \
-       -o -name "*.txt" -o -name "*.d4" -o -name "*.cfg" -o -name "*.s" \) \
+       -o -name "*.txt" -o -name "*.d4" -o -name "*.cfg" \) \
        -exec "$@" {} \;
